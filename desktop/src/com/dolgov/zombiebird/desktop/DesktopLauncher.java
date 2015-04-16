debe25e00635a97bf22c6d3cb5280c3c46745a50
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-17 00:47:02 +0400

    Initial commit

---
+++ desktop/src/com/dolgov/zombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dolgov.zombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dolgov.zombiebird.ZBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
