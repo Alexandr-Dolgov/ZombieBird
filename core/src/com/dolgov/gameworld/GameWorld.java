f858abbefee9cd8e133daa35cc7891b2456840da
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-18 00:29:26 +0400

    Орисовываем бегущий серый прямоугольник в красной рамке. Прямоугольник, это объект игрового мира, и состояние прямоугольника обновляется при обновлении игрового мира. Класс GameRender, лишь рисует прямоугольник из мира.

--- core/src/com/dolgov/gameworld/GameWorld.java
+++ core/src/com/dolgov/gameworld/GameWorld.java
@@ -1,12 +1,24 @@
 package com.dolgov.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
 
 /**
  * Created by Alexandr on 17.04.2015.
  */
 public class GameWorld {
+
+    private Rectangle rect = new Rectangle(0, 0, 17, 12);
+
     public void update(float delta){
         Gdx.app.log("GameWorld", "updated");
+        rect.x++;
+        if (rect.x > 137){
+            rect.x = 0;
+        }
+    }
+
+    public Rectangle getRect(){
+        return rect;
     }
 }
