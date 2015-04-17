480ff091391c666520ffb56987a3fb3340cb3dc6
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-17 23:59:25 +0400

    LibGDX движок 3D, а наша игры 2D. Но мы просто используем соответствющую камеру: OrthographicCamera Причем ее разрешение ровно в два раза и по высоте, и по ширине, меньше чем размеры изображения, так что все будет просто в два раза увеличивать при отображении.

--- core/src/com/dolgov/gameworld/GameRender.java
+++ core/src/com/dolgov/gameworld/GameRender.java
@@ -1,6 +1,7 @@
 package com.dolgov.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 /**
  * Created by Alexandr on 17.04.2015.
@@ -8,9 +9,12 @@ import com.badlogic.gdx.Gdx;
 public class GameRender {
 
     private GameWorld world;
+    private OrthographicCamera camera;
 
     public GameRender(GameWorld world){
         this.world = world;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, 136, 204);
     }
 
     public void render(){
