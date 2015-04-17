4f626fec2736297fe90496dad4765ae61e9f7a7e
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-17 23:28:02 +0400

    Добавили заглушку метода обновления игрового мира.

--- core/src/com/dolgov/gameworld/GameWorld.java
+++ core/src/com/dolgov/gameworld/GameWorld.java
@@ -1,7 +1,12 @@
 package com.dolgov.gameworld;
 
+import com.badlogic.gdx.Gdx;
+
 /**
  * Created by Alexandr on 17.04.2015.
  */
 public class GameWorld {
+    public void update(float delta){
+        Gdx.app.log("GameWorld", "updated");
+    }
 }
