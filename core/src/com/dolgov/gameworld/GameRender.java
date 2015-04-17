e7e804a4d3a109563c1a98949534517bf13deccf
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-17 23:29:20 +0400

    Добавили заглушку метода отрисовка игрового мира.

--- core/src/com/dolgov/gameworld/GameRender.java
+++ core/src/com/dolgov/gameworld/GameRender.java
@@ -1,7 +1,12 @@
 package com.dolgov.gameworld;
 
+import com.badlogic.gdx.Gdx;
+
 /**
  * Created by Alexandr on 17.04.2015.
  */
 public class GameRender {
+    public void render(){
+        Gdx.app.log("GameRender", "render");
+    }
 }
