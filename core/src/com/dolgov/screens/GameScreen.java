288d0eef1c8d2c6b97eb1eb16ee18d1c59be8652
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-17 23:36:44 +0400

    И наконец вызываем методы обновления и отрисовки игрового мира, при отрисовке экрана игры.

--- core/src/com/dolgov/screens/GameScreen.java
+++ core/src/com/dolgov/screens/GameScreen.java
@@ -27,11 +27,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
-        Gdx.gl.glClearColor(10 / 255.0f, 15 / 255.0f, 230 / 255.0f, 1f);
-        // Fills the screen with the selected color
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        Gdx.app.log("GameScreen FPS", (1/delta) + "");
+        world.update(delta);//delta исползуем чтобы сделать обновление мира зависимым от FPS
+        render.render();
     }
 
     @Override
