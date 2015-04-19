37dff653b7d72d79a8dc90b052c1b4c703bf727b
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-20 02:34:56 +0400

    Вызываем загрузку и выгрузку ресурсов.

--- core/src/com/dolgov/zombiebird/ZBGame.java
+++ core/src/com/dolgov/zombiebird/ZBGame.java
@@ -3,11 +3,20 @@ package com.dolgov.zombiebird;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.dolgov.screens.GameScreen;
+import com.dolgov.zbhelpers.AssetLoader;
 
 public class ZBGame extends Game {
+
     @Override
     public void create() {
         Gdx.app.log("ZBGame", "created");
+        AssetLoader.load();
         setScreen(new GameScreen());
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        AssetLoader.dispose();
     }
 }
