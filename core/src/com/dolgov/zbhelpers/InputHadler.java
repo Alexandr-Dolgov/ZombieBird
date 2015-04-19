9e1539c92eb18f0c46359625ddc3dae66de5032e
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-19 23:30:19 +0400

    При касании вызываем соотв. обработчик.

--- core/src/com/dolgov/zbhelpers/InputHadler.java
+++ core/src/com/dolgov/zbhelpers/InputHadler.java
@@ -31,7 +31,8 @@ public class InputHadler implements InputProcessor{
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
+        bird.onClick();
+        return true;    //сообщаем что обработали касание
     }
 
     @Override
