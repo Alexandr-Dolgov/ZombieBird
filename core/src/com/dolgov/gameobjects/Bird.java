947f2819ae9c63b6a9eb86f33270185740c919e6
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-19 22:27:49 +0400

    У нашей птицы будет три двумерных вектора: позиция, скорость, ускорения. Каждый по х и по у. А также у нашей птицы будет угол наклона rotation, и естественно размер по ширине и высоте.

--- core/src/com/dolgov/gameobjects/Bird.java
+++ core/src/com/dolgov/gameobjects/Bird.java
@@ -1,7 +1,16 @@
 package com.dolgov.gameobjects;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by Alexandr on 19.04.2015.
  */
 public class Bird {
+    private Vector2 position;       //двухмерный вектор, хранящий позицию по х и у для нашей птицы
+    private Vector2 velocity;       //сответственно вектор скоростей по х и у
+    private Vector2 acceleration;   //и вектор ускорейний по х и у
+
+    private float rotation;
+    private int width;
+    private int height;
 }
