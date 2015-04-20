5a4f69456cc5acd2b94b07539d6ca565b9c31349
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-20 19:40:32 +0400

    Отрисовали трубы с травой

--- core/src/com/dolgov/gameworld/GameRender.java
+++ core/src/com/dolgov/gameworld/GameRender.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.dolgov.gameobjects.Bird;
+import com.dolgov.gameobjects.Grass;
+import com.dolgov.gameobjects.Pipe;
+import com.dolgov.gameobjects.ScrollHandler;
 import com.dolgov.zbhelpers.AssetLoader;
 
 /**
@@ -21,7 +24,11 @@ public class GameRender {
     private int midPointY;
     private int gameHeight;
 
+    // Game Objects
     private Bird bird;
+    private ScrollHandler scroller;
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
 
     public GameRender(GameWorld world, int gameHeight, int midPointY){
         this.world = world;
@@ -43,6 +50,12 @@ public class GameRender {
 
     private void initGameObjects() {
         bird = world.getBird();
+        scroller = world.getScroller();
+        frontGrass = scroller.getFrontGrass();
+        backGrass = scroller.getBackGrass();
+        pipe1 = scroller.getPipe1();
+        pipe2 = scroller.getPipe2();
+        pipe3 = scroller.getPipe3();
     }
 
     public void render(float runTime){
@@ -75,8 +88,15 @@ public class GameRender {
         batcher.disableBlending();
         batcher.draw(AssetLoader.bg, 0, midPointY + 23, 136, 43);
 
-        // Птичке нужна прозрачность, поэтому включаем ее
+        // 1. Отрисовываем Grass
+        drawGrass();
+
+        // 2. Отрисовываем Pipes
+        drawPipes();
+
+        // 3. Отрисовываем Skulls (необходима прозрачность)
         batcher.enableBlending();
+        drawSkulls();
 
         if (bird.shouldntFlap()) {
             batcher.draw(
@@ -101,4 +121,52 @@ public class GameRender {
         // Заканчиваем SpriteBatch
         batcher.end();
     }
+
+    private void drawGrass() {
+        // отрисуем траву
+        batcher.draw(AssetLoader.grass, frontGrass.getX(), frontGrass.getY(),
+                frontGrass.getWidth(), frontGrass.getHeight());
+        batcher.draw(AssetLoader.grass, backGrass.getX(), backGrass.getY(),
+                backGrass.getWidth(), backGrass.getHeight());
+    }
+
+    private void drawSkulls() {
+        // Временный код, извините за кашу :)
+        // Мы это починим, как только закончим с Pipe классом.
+
+        batcher.draw(AssetLoader.skullUp, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() - 14, 24, 14);
+        batcher.draw(AssetLoader.skullDown, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() + 45, 24, 14);
+
+        batcher.draw(AssetLoader.skullUp, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() - 14, 24, 14);
+        batcher.draw(AssetLoader.skullDown, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() + 45, 24, 14);
+
+        batcher.draw(AssetLoader.skullUp, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() - 14, 24, 14);
+        batcher.draw(AssetLoader.skullDown, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() + 45, 24, 14);
+    }
+
+    private void drawPipes() {
+        // Временный код, извините за кашу :)
+        // Мы это починим, как только закончим с Pipe классом.
+        batcher.draw(AssetLoader.bar, pipe1.getX(), pipe1.getY(), pipe1.getWidth(),
+                pipe1.getHeight());
+        batcher.draw(AssetLoader.bar, pipe1.getX(), pipe1.getY() + pipe1.getHeight() + 45,
+                pipe1.getWidth(), midPointY + 66 - (pipe1.getHeight() + 45));
+
+        batcher.draw(AssetLoader.bar, pipe2.getX(), pipe2.getY(), pipe2.getWidth(),
+                pipe2.getHeight());
+        batcher.draw(AssetLoader.bar, pipe2.getX(), pipe2.getY() + pipe2.getHeight() + 45,
+                pipe2.getWidth(), midPointY + 66 - (pipe2.getHeight() + 45));
+
+        batcher.draw(AssetLoader.bar, pipe3.getX(), pipe3.getY(), pipe3.getWidth(),
+                pipe3.getHeight());
+        batcher.draw(AssetLoader.bar, pipe3.getX(), pipe3.getY() + pipe3.getHeight() + 45,
+                pipe3.getWidth(), midPointY + 66 - (pipe3.getHeight() + 45));
+    }
+
 }
