ed03e862b27bbea94950cd4267a8bd456385097d
Alexandr-Dolgov <samara-fv@yandex.ru>
2015-04-20 19:27:02 +0400

    Связали обработчик прокрутки с игровым миром

--- core/src/com/dolgov/gameworld/GameWorld.java
+++ core/src/com/dolgov/gameworld/GameWorld.java
@@ -1,6 +1,7 @@
 package com.dolgov.gameworld;
 
 import com.dolgov.gameobjects.Bird;
+import com.dolgov.gameobjects.ScrollHandler;
 
 /**
  * Created by Alexandr on 17.04.2015.
@@ -8,16 +9,23 @@ import com.dolgov.gameobjects.Bird;
 public class GameWorld {
 
     private Bird bird;
+    private ScrollHandler scroller;
 
     public GameWorld(int midPointY) {
         bird = new Bird(33, midPointY - 5, 17, 12);
+        scroller = new ScrollHandler(midPointY + 66);
     }
 
     public void update(float delta){
         bird.update(delta);
+        scroller.update(delta);
     }
 
     public Bird getBird(){
         return bird;
+    }
+
+    public ScrollHandler getScroller() {
+        return scroller;
     }
 }
